2644cd7b2bb81e37c88dfdcab9277460704ea7b7
Puneeth7kumar <116489871+Puneeth7kumar@users.noreply.github.com>
2024-08-26 12:10:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './components/login';
 import Register from './components/register';
 import Dashboard from './components/dashboard';
-
-// Assuming you have a Dashboard component
-
 function App() {
   return (
     <Router>
-
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        {/* Add additional routes here */}
       </Routes>
     </Router>
   );
