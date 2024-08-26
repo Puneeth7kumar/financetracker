64cabeb8121fd09c9e92153e6e9348b762a81746
Puneeth7kumar <116489871+Puneeth7kumar@users.noreply.github.com>
2024-08-26 12:19:14 +0530

    Update register.js

--- src/components/register.js
+++ src/components/register.js
@@ -1,4 +1,4 @@
-// src/components/Register.js
+
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
